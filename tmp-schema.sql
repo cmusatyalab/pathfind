6247ba333add9683dbd2538580d4078ce79bc21b
Adam Goode <agoode@andrew.cmu.edu>
2010-05-27 10:22:11 -0400

    Add initial temporary sql schema

---
+++ tmp-schema.sql
@@ -0,0 +1,40 @@
+drop table roi_slide;
+drop table annotation;
+drop table roi;
+drop table author;
+
+create table author (
+    id int auto_increment primary key,
+    name text not null
+  )
+  engine = InnoDB;
+
+create table roi (
+    id int auto_increment primary key,
+    path text not null,
+    author_id int not null references author,
+    timestamp timestamp not null default current_timestamp,
+    deleted bit not null default 0
+  )
+  engine = InnoDB;
+
+create table annotation (
+    id int auto_increment primary key,
+    roi_id int not null references roi,
+    author_id int not null references author,
+    timestamp timestamp not null default current_timestamp,
+    text text not null
+  )
+  engine = InnoDB;
+
+create table roi_slide (
+    id int auto_increment primary key,
+    quickhash1 text not null,
+    roi_id int not null references roi
+  )
+  engine = InnoDB;
+
+
+
+
+insert into author (name) values ('Adam Goode');
